7bfb19dcb30bb5c8f937d26d3df5dcaa054693a1
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2016-10-10 19:16:42 +0300

    starting

---
+++ examples/example15/tools.php
@@ -0,0 +1,10 @@
+<?
+define('APP_ID', 'local.564084a58ad8b0.76333855');
+define('APP_SECRET_CODE', '1798efe41b0c132c1ea756beae819560');
+define('APP_REG_URL', 'http://b24go.com/rating/index.php');
+
+require_once('bitrix24.php');
+require_once('bitrix24exception.php');
+require_once('bitrix24entity.php');
+require_once('bitrix24user.php');
+
